9752ed5184b8a8f840f0154edcec7a97ba4ea159
aniket.nimse <aniket.nimse@techbulls.com>
2026-01-28 13:59:03 +0530

    added security config with defalut basic auth

---
+++ src/main/java/com/example/springSecurityDemo/Configuration/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.springSecurityDemo.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http){
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+//        http.formLogin(Customizer.withDefaults());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
