6f831eb1c7074958fc522b5a9565b4510ec46c7b
aniket.nimse <aniket.nimse@techbulls.com>
2026-03-11 20:54:21 +0530

    problem for frequency of words in sentence

---
+++ src/main/java/com/example/codingExamples/FrequencyOfWordsInSentence.java
@@ -0,0 +1,32 @@
+package com.example.codingExamples;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class FrequencyOfWordsInSentence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter sentence to calculate words in sentence");
+        String sentence = sc.nextLine().trim();
+
+        sentence = sentence.toLowerCase();
+
+        if(sentence.isEmpty()){
+            System.out.println("sorry! empty sentence.");
+            return;
+        }
+
+        String[] words = sentence.split("\\s+");
+
+        LinkedHashMap<String , Integer> wordFrequencyMap = new LinkedHashMap<>();
+
+        for(String word: words){
+            wordFrequencyMap.put(word , wordFrequencyMap.getOrDefault(word , 0)+1);
+        }
+
+        for(Map.Entry<String,Integer> m : wordFrequencyMap.entrySet()){
+            System.out.println(m.getKey() + " --> " + m.getValue());
+        }
+    }
+}
